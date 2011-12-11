9c69287c236cec31037ed2b9c363e9d9253eda6f
Rakhmad Azhari <rakhmad@csui01.org>
2011-12-11 22:20:07 +0700

    Initializing Ticket Controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,11 @@
 class TicketsController < ApplicationController
+  before_filter :find_project
+  def new
+    @ticket = @project.tickets.build
+  end
+  
+  private 
+    def find_project
+      @project = Project.find(params[:project_id])
+    end
 end
