eafa7e1f7599d52892a7b521f0a7460933df710a
Rakhmad Azhari <rakhmad@csui01.org>
2011-12-11 22:12:17 +0700

    Adding Controller Ticket

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,9 @@ Ticketee::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'projects#index'
-  resources :projects
+  resources :projects do
+    resources :tickets
+  end
 
   # See how all your routes lay out with "rake routes"
 
