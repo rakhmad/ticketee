a94b58eb95e0be00d7b77ab23a52b8e294cdd1e0
Rakhmad Azhari <rakhmad@csui01.org>
2011-12-11 11:42:45 +0700

    Adding Model Project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,9 @@
 class ProjectsController < ApplicationController
+  def index
+  end
+  
+  def new
+    @project = Project.new
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ Ticketee::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'projects#index'
+  resources :projects
 
   # See how all your routes lay out with "rake routes"
 
